221901eb4a94d64bb774547e2ce870f145089129
mkarakaya <mkarakaya604@gmail.com>
2021-11-12 12:26:44 +0300

    add words to memorize.txt file that you don't know

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/manifoldco/promptui"
 )
@@ -15,28 +16,60 @@ func main() {
 		log.Fatalf("err to open file: %v", err)
 	}
 
+	fileMemorize, err := os.OpenFile("memorize.txt", os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatalf("err to open file: %v", err)
+	}
+
+	clear := confirm("Do you wanna reset words to memorize?")
+	if clear == "Yes" {
+		os.Truncate("memorize.txt", 0)
+	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	counter := 0
+	clearScreen()
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+
+		counter += 1
+		fmt.Println(scanner.Text(), counter)
+		status := confirm("Do you know this word?")
+		if status == "No" {
+			fileMemorize.WriteString(scanner.Text() + "\n")
+			fmt.Println("i don't know this word")
+
+		} else if status == "Yes" {
+			fmt.Println("i know this word")
+		}
+		clearScreen()
 
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("scanner err: %v", err)
 	}
+	defer fileMemorize.Close()
 
 }
-
-func confirm() bool {
+func clearScreen() {
+	//For Linux & Darwin
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+func confirm(label string) string {
 	prompt := promptui.Select{
-		Label: "Select[Yes/No]",
-		Items: []string{"Yes", "No"},
+		Label: label,
+		Items: []string{"Yes", "No", "Cancel"},
 	}
 	_, result, err := prompt.Run()
 	if err != nil {
 		log.Fatalf("prompt failed: %v\n", err)
 	}
-	return result == "Yes"
+	if result == "Cancel" {
+		os.Exit(0)
+	}
+	return result
 }
